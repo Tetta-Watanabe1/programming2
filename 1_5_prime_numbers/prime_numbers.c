0e65a4050a8b4146172db84e5d5011cdad88ffc0
Tetta <syouraiyuubou15@gmail.com>
2025-10-10 18:19:26 +0900

    1_5

--- 1_5_prime_numbers/prime_numbers.c
+++ 1_5_prime_numbers/prime_numbers.c
@@ -1,27 +1,27 @@
 /**
 
-氏名: 
+氏名:
 
-学生番号: 
+学生番号:
 
-作成日: 
+作成日:
 
-入出力の説明: 
+入出力の説明:
 
-動きの説明: 
+動きの説明:
 
-感想: 
+感想:
 
 */
 
-#include<stdio.h>
-int main(){
-  int max, min, check=1, prime_numbers[10000], quantity, j=0;
-  for(;min<=max;min++, j++){
-    for(int i = 2; i<=min; i++)
-      if(min%i!=0)
-        check=0;
-    if(check)
-      prime_number[j]=min;
-      
+#include <stdio.h>
+int main() {
+  int max, min, check = 1, prime_numbers[10000], quantity, j = 0;
+  for (; min <= max; min++, j++) {
+    for (int i = 2; i <= min; i++)
+      if (min % i != 0)
+        check = 0;
+    if (check)
+      prime_numbers[j] = min;
+  }
 }
