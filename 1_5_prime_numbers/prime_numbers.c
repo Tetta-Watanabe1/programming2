bbaceaaa2ea8975ac291043f1b938d9ff6fb84b1
てー <230082730+Tetta-Watanabe1@users.noreply.github.com>
2025-10-11 09:19:42 +0900

    without printf

--- 1_5_prime_numbers/prime_numbers.c
+++ 1_5_prime_numbers/prime_numbers.c
@@ -16,12 +16,15 @@
 
 #include <stdio.h>
 int main() {
-  int max, min, check = 1, prime_numbers[10000], quantity, j = 0;
-  for (; min <= max; min++, j++) {
+  int max, min, check, prime_numbers[10000], quantity, j = 0;
+  for (; min <= max; min++) {
+    check = 1;
     for (int i = 2; i <= min; i++)
       if (min % i != 0)
         check = 0;
-    if (check)
+    if (check){
       prime_numbers[j] = min;
+      j++;
+    }
   }
 }
