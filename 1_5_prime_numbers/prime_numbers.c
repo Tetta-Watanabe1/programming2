d0910266cf55224fa9d24a42e161fb880ea9ea7a
Tetta <syouraiyuubou15@gmail.com>
2025-10-14 20:29:18 +0900

    finish 1_5

--- 1_5_prime_numbers/prime_numbers.c
+++ 1_5_prime_numbers/prime_numbers.c
@@ -16,10 +16,10 @@
 
 #include <stdio.h>
 int main() {
-  int max, min, check, prime_numbers[10000], quantity, i, j = 0;
+  int max, min, check, prime_numbers[10000], i, j = 0;
   scanf("%d", &min);
   scanf("%d", &max);
-  for (; min <= max; min++) {
+  for (; min < max; min++) {
     check = 1;
     for (i = 2; i < min; i++)
       if ((min % i) == 0)
---
+++ 1_5_prime_numbers/tests/prime_numbers_prev.c
@@ -0,0 +1,34 @@
+/**
+
+氏名:
+
+学生番号:
+
+作成日:
+
+入出力の説明:
+
+動きの説明:
+
+感想:
+
+*/
+
+#include <stdio.h>
+int main() {
+  int max, min, check, prime_numbers[10000], i, j = 0;
+  scanf("%d", &min);
+  scanf("%d", &max);
+  for (; min < max; min++) {
+    check = 1;
+    for (i = 2; i < min; i++)
+      if ((min % i) == 0)
+        check = 0;
+    if (check) {
+      prime_numbers[j] = min;
+      j++;
+    }
+  }
+  for (i = 0; i < j; i++)
+    printf("%d\n", prime_numbers[i]);
+}
