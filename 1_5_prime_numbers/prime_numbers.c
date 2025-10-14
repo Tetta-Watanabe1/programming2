38c01db9ca092829cadb5d9b727aec71e812964d
Tetta <syouraiyuubou15@gmail.com>
2025-10-14 19:35:57 +0900

    still

--- 1_5_prime_numbers/prime_numbers.c
+++ 1_5_prime_numbers/prime_numbers.c
@@ -17,16 +17,18 @@
 #include <stdio.h>
 int main() {
   int max, min, check, prime_numbers[10000], quantity, i, j = 0;
+  scanf("%d", &min);
+  scanf("%d", &max);
   for (; min <= max; min++) {
     check = 1;
     for (i = 2; i <= min; i++)
       if (min % i != 0)
         check = 0;
-    if (check){
+    if (check) {
       prime_numbers[j] = min;
       j++;
     }
   }
-  for(i=0;i<j;i++)
-    printf("%d\n", prime_numbers[j])
+  for (i = 0; i < j; i++)
+    printf("%d\n", prime_numbers[i]);
 }
