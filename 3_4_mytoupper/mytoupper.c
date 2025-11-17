466dcc27933ffa5307502dc22896a542a3a35401
Tetta <syouraiyuubou15@gmail.com>
2025-11-17 23:10:19 +0900

    3_4 complete

--- 3_4_mytoupper/mytoupper.c
+++ 3_4_mytoupper/mytoupper.c
@@ -1,27 +1,34 @@
 /**
 
-氏名: 
+氏名: 渡邊鉄太
 
-学生番号: 
+学生番号: B255451
 
-作成日: 
+作成日: 2025-11-17
 
-入出力の説明: 
+入出力の説明:
 
-動きの説明: 
+動きの説明:
 
-感想: 
+感想:
 
 */
 #include <stdio.h>
 
 /* lower2upper関数をこの辺りに定義する */
+char lower2upper(char c) {
+  if ('a' <= c && c <= 'z') {
+    return c - 1;
+  }
+  return c;
+}
 
 int main() {
   char c;
   while ((c = getchar()) != '\n') {
 
     /* ここでlower2upper関数を呼び出す．引数は変数c．戻り値はcに代入する */
+    c = lower2upper(c);
 
     printf("%c", c);
   }
