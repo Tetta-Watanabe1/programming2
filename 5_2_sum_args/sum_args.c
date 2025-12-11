39a05541ea16e022dd40ed8c7286ce348c226399
Ubuntu <syouraiyuubou15@gmail.com>
2025-12-12 07:08:27 +0900

    5_2

--- 5_2_sum_args/sum_args.c
+++ 5_2_sum_args/sum_args.c
@@ -13,3 +13,12 @@
 感想: 
 
 */
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(int argc, char *argv[]){
+	int sum = 0;
+	for(int i = 1;i < argc;i++)
+		sum += atoi(argv[i]);
+	printf("%d\n", sum);
+}
