ffc34b931fd7383afbd2ba98f4292b2bdd3d557c
Tetta <syouraiyuubou15@gmail.com>
2025-12-12 04:51:54 +0900

    55555

--- 5_3_sort_args/sort_args.c
+++ 5_3_sort_args/sort_args.c
@@ -19,14 +19,14 @@ void dictional_sort(char *a, char *b, int check){
 		i = 0;
 
 	while(0){
-		if(a[i] == '\0' || b[] = '\0')
+		if(a[i] == '\0' || b[i] = '\0')
 			break;
 		else if(a[i] > b[i])
 			break;
 		else if(b[i] > a[i]){
-			char temp[] = a[];
-			a[] = b[];
-			b[] = temp[];
+			char temp = a;
+			a = b;
+			b = temp;
 		}
 		else if(a[i] == b[i])
 			dictional_sort(a, b, 0);
