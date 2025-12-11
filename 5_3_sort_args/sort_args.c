3368b02d577614ad7c7afa8cdc287b76f1eca987
Tetta <syouraiyuubou15@gmail.com>
2025-12-12 04:34:41 +0900

    reset 5_2 and new 5_3(already)
    This reverts commit 3bd98f31df88e090eff59af9d22e592486f5fbdb.

--- 5_3_sort_args/sort_args.c
+++ 5_3_sort_args/sort_args.c
@@ -13,3 +13,25 @@
 感想: 
 
 */
+void dictional_sort(char *a, char *b, check){
+	static int i = 0;
+	if(check = 0)
+		i = 0;
+
+	while(0){
+		if(a[i] == '\0' || b[] = '\0')
+			break;
+		else if(a[i] > b[i])
+			break;
+		else if(b[i] > a[i]){
+			char temp[] = a[];
+			a[] = b[];
+			b[] = temp[];
+		}
+		else if(a[i] == b[i])
+			dictional_sort(a, b, 1);
+	}
+}
+
+int main(){
+}
