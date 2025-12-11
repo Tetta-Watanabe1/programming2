c7312810cdfdae6c0406cfa47763bb669bfb8a42
Tetta <syouraiyuubou15@gmail.com>
2025-12-12 03:33:05 +0900

    5_1

---
+++ 5_1_args/tests/args_prev.c
@@ -0,0 +1,24 @@
+/**
+
+氏名: 
+
+学生番号: 
+
+作成日: 
+
+入出力の説明: 
+
+動きの説明: 
+
+感想: 
+
+*/
+
+#include<stdio.h>
+
+int main(int argc, char *argv[]) {
+	printf("%d\n", argc);
+	for(int i = 1;argv[i] != NULL;i++)
+		printf("%d: %s\n", i, argv[i]);
+  return 0;
+}
